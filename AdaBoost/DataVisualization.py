bc38361938521b18048f64a5fbb4271ef4c07260
Gongas <goncalo.mesteves03@gmail.com>
2024-04-14 20:37:33 +0100

    Added a Function to Plot the ROC Curve

--- AdaBoost/DataVisualization.py
+++ AdaBoost/DataVisualization.py
@@ -11,6 +11,9 @@ This File contains multiple functions used to Visualize Data:
 
     -> Display_Confusion_Matrix(fit_model, X_Test, y_Test, labels):
         - Displays a Confusion Matrix of a given Model
+    
+    -> def Plot_ROC_Curve(fit_model, X_Test, Y_Test):
+        - Plots the ROC Curve
 
 '''
 
@@ -37,4 +40,38 @@ def Display_Confusion_Matrix(fit_model, X_Test, y_Test, labels):
     ax.yaxis.set_ticklabels(labels)
 
     # Showing Final Product
+    plt.show()
+
+def Plot_ROC_Curve(fit_model, X_Test, Y_Test):
+    # Predict Probability of belonging to a certain class
+    Y_Pred_Proba = fit_model.predict_proba(X_Test)[::,1]
+    
+    # Getting the ROC Curve
+    false_positive_rate, true_positive_rate, _ = roc_curve(Y_Test, Y_Pred_Proba)
+    
+    # Calculating the Area Under Curve
+    AUC = roc_auc_score(Y_Test, Y_Pred_Proba)
+
+    # Adds an Axes to the current figure 
+    ax = plt.subplot()
+    
+    # Setting the Title
+    ax.set_title('ROC Curve')
+
+    # Setting the names to the X & Y labels
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
+    
+    # Plotting the Results
+    plt.plot(false_positive_rate,
+             true_positive_rate,
+             label=f"AUC = {AUC:1.3f}",
+             color="darkblue",
+             linestyle='-',
+             linewidth=1.4)
+    
+    # Creating the y=x function into the graph
+    ax.axline((0, 0), slope=1, label="Chance Level (AUC = 0.5)", color="darkred", linestyle='--')
+    
+    plt.legend(loc=4)
     plt.show()
