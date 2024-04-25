39cbe154382eb37178b828580dcfd5daad07250c
Gongas <goncalo.mesteves03@gmail.com>
2024-04-25 17:54:19 +0100

    Added Loss Function Implementation into the Main AdaBoost Class

--- AdaBoost/AdaBoostStudy/AdaBoost.py
+++ AdaBoost/AdaBoostStudy/AdaBoost.py
@@ -13,36 +13,6 @@ as well as some functions to compute values along the training and testing of th
 
 '''
 
-# Compute error rate, alpha and w
-def compute_error(y, y_pred, w_i):
-    '''
-    Calculate the error rate of a weak classifier m. Arguments:
-    y: actual target value
-    y_pred: predicted value by weak classifier
-    w_i: individual weights for each observation
-    
-    Note that all arrays should be the same length
-    '''
-    return (sum(w_i * (np.not_equal(y, y_pred)).astype(int)))/sum(w_i)
-
-def compute_alpha(error):
-    '''
-    Calculate the weight of a weak classifier m in the majority vote of the final classifier. This is called
-    alpha in chapter 10.1 of The Elements of Statistical Learning. Arguments:
-    error: error rate from weak classifier m
-    '''
-    return np.log((1 - error) / error)
-
-def update_weights(w_i, alpha, y, y_pred):
-    ''' 
-    Update individual weights w_i after a boosting iteration. Arguments:
-    w_i: individual weights for each observation
-    y: actual target value
-    y_pred: predicted value by weak classifier  
-    alpha: weight of weak classifier used to estimate y_pred
-    '''  
-    return w_i * np.exp(alpha * (np.not_equal(y, y_pred)).astype(int))
-
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
@@ -50,14 +20,79 @@ __version__ = "2.0.1"
 
 # Define AdaBoost class
 class AdaBoost(BaseEstimator, ClassifierMixin):
-    def __init__(self, weak_learner=DecisionTreeClassifier, weak_learner_hyperparameters={'max_depth':1}):
+    def __init__(self, weak_learner=None, weak_learner_hyperparameters=None, loss_function=None, learning_rate=None):
         self.alphas = []
-        self.weak_learner = weak_learner
-        self.weak_learner_hyperparameters = weak_learner_hyperparameters
+        self.weak_learner = DecisionTreeClassifier if weak_learner is None else weak_learner
+        self.weak_learner_hyperparameters = {'max_depth':1} if weak_learner_hyperparameters is None else weak_learner_hyperparameters
+        self.loss_function = 'exponential' if loss_function is None else loss_function
+        self.learning_rate = 0.01 if learning_rate is None else learning_rate
+        
         self.G_M = []
         self.M = None
+
         self.training_errors = []
         self.prediction_errors = []
+
+    def compute_error(self, y_true, y_pred, w_i):
+        '''
+        Calculate the error rate of a weak classifier m. Arguments:
+        y: actual target value
+        y_pred: predicted value by weak classifier
+        w_i: individual weights for each observation
+        
+        Note that all arrays should be the same length
+        '''
+        # Assuming y_true is in {-1, 1}
+        return np.sum(w_i * (y_pred != y_true))
+
+    def compute_alpha(self, error):
+        '''
+        Calculate the weight of a weak classifier m in the majority vote of the final classifier. This is called
+        alpha in chapter 10.1 of The Elements of Statistical Learning. Arguments:
+        error: error rate from weak classifier m
+        '''
+        
+        # Exponential Loss
+        if self.loss_function == 'exponential':
+            return np.log((1 - error) / error)
+        
+        # Logistic Loss and Hinge Loss
+        elif self.loss_function == 'logistic' or self.loss_function == 'hinge':
+            return 0.5 * np.log((1 - error) / error)
+        
+        # Squared Loss
+        elif self.loss_function == 'squared':
+            return self.learning_rate
+
+    def update_weights(self, w_i, alpha, y_true, y_pred):
+        ''' 
+        Update individual weights w_i after a boosting iteration. Arguments:
+        w_i: individual weights for each observation
+        y: actual target value
+        y_pred: predicted value by weak classifier  
+        alpha: weight of weak classifier used to estimate y_pred
+        '''
+
+        # Default Exponential Loss
+        if self.loss_function == 'exponential':  
+            w_i *= np.exp(alpha * (np.not_equal(y_true, y_pred)).astype(int))
+        
+        # Logistic Loss
+        elif self.loss_function == 'logistic':
+            w_i *= np.exp(-alpha * y_true * y_pred) / (2 - np.sum(w_i * (y_pred != y_true)))
+        
+        # Hinge Loss
+        elif self.loss_function == 'hinge':
+            incorrect = (y_true * y_pred < 1)
+            w_i[incorrect] *= np.exp(alpha)
+
+        # Squared Loss
+        elif self.loss_function == 'squared':
+                # Update weights based on squared error
+                w_i *= np.exp(alpha * (1 - y_true * y_pred)**2)
+        
+        # Normalize weights
+        return w_i / np.sum(w_i)
 
     def fit(self, X, y, M = 100):
         '''
@@ -67,6 +102,10 @@ class AdaBoost(BaseEstimator, ClassifierMixin):
         M: number of boosting rounds. Default is 100 - integer
         '''
         
+        # Making sure the Loss Function is Valid
+        if (self.loss_function not in set(['exponential', 'logistic', 'hinge', 'squared'])):
+            raise Exception('Instance of AdaBoost initialized with invalid Loss Function!')
+
         # Clear before calling
         self.alphas = []
         self.training_errors = []
@@ -80,7 +119,7 @@ class AdaBoost(BaseEstimator, ClassifierMixin):
                 w_i = np.ones(len(y)) * 1 / len(y)  # At m = 0, weights are all the same and equal to 1 / N
             else:
                 # (d) Update w_i
-                w_i = update_weights(w_i, alpha_m, y, y_pred)
+                w_i = self.update_weights(w_i, alpha_m, y, y_pred)
             
             # (a) Fit weak classifier and predict labels
             G_m = self.weak_learner(**self.weak_learner_hyperparameters)  # By Default uses a Stump: Two terminal-node classification tree
@@ -90,11 +129,11 @@ class AdaBoost(BaseEstimator, ClassifierMixin):
             self.G_M.append(G_m) # Save to list of weak classifiers
 
             # (b) Compute error
-            error_m = compute_error(y, y_pred, w_i)
+            error_m = self.compute_error(y, y_pred, w_i)
             self.training_errors.append(error_m)
 
             # (c) Compute alpha
-            alpha_m = compute_alpha(error_m)
+            alpha_m = self.compute_alpha(error_m)
             self.alphas.append(alpha_m)
 
         assert len(self.G_M) == len(self.alphas)
