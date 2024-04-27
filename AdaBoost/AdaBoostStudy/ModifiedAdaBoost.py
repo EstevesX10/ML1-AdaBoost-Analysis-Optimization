5bbff31337fc132be1679f7b1ef6bae674da9ced
Gongas <goncalo.mesteves03@gmail.com>
2024-04-28 00:29:55 +0100

    Added AdaBoost with SVM

--- AdaBoost/AdaBoostStudy/ModifiedAdaBoost.py
+++ AdaBoost/AdaBoostStudy/ModifiedAdaBoost.py
@@ -1,6 +1,7 @@
 import numpy as np
-from sklearn.linear_model import (Perceptron)
 from sklearn.tree import (DecisionTreeClassifier)
+from sklearn.svm import (SVC)
+from sklearn.linear_model import (Perceptron)
 from .AdaBoost import (AdaBoost)
 
 '''
@@ -15,9 +16,9 @@ This File contains Modified Versions of the AdaBoost Algorithm based on:
 
 '''
 
-class AdaBoostPerceptron(AdaBoost):
-    def __init__(self):
-        super().__init__(weak_learner=Perceptron, weak_learner_hyperparameters={})
+class AdaBoostTunedDT(AdaBoost):
+    def __init__(self, weak_learner_hyperparameters={'max_depth':1}):
+        super().__init__(weak_learner=DecisionTreeClassifier, weak_learner_hyperparameters=weak_learner_hyperparameters)
 
     def fit(self, X, y, M = 100):
         return super().fit(X, y, M)
@@ -28,9 +29,22 @@ class AdaBoostPerceptron(AdaBoost):
     def predict_proba(self, X):
         return super().predict_proba(X)
 
-class AdaBoostTunedDT(AdaBoost):
-    def __init__(self, weak_learner_hyperparameters={'max_depth':1}):
-        super().__init__(weak_learner=DecisionTreeClassifier, weak_learner_hyperparameters=weak_learner_hyperparameters)
+class AdaBoostSVM(AdaBoost):
+    def __init__(self):
+        super().__init__(weak_learner=SVC, weak_learner_hyperparameters={'kernel':'linear'})
+
+    def fit(self, X, y, M = 100):
+        return super().fit(X, y, M)
+
+    def predict(self, X):
+        return super().predict(X)
+
+    def predict_proba(self, X):
+        return super().predict_proba(X)
+
+class AdaBoostPerceptron(AdaBoost):
+    def __init__(self):
+        super().__init__(weak_learner=Perceptron, weak_learner_hyperparameters={})
 
     def fit(self, X, y, M = 100):
         return super().fit(X, y, M)
