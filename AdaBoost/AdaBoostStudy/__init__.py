39faf7fc95fa4b81fcaa6d3d0aff1dfc74202960
Gongas <goncalo.mesteves03@gmail.com>
2024-05-08 23:36:04 +0100

    Added a custom import system to the package

---
+++ AdaBoost/AdaBoostStudy/__init__.py
@@ -0,0 +1,11 @@
+# Defining which submodules to import when using from <package> import *
+__all__ = ["AdaBoost", "AdaBoostTunedDT", "AdaBoostPerceptron",
+           "Fetch_Dataset", "Fetch_X_y",
+           "Plot_Scatterplots", "Display_Confusion_Matrix", "Plot_ROC_Curve", "Plot_Model_Stats", "Model_Accuracies_Per_Dataset", "Plot_Critial_Difference_Diagram",
+           "Save_json_file", "Load_json_file", "Manage_Results", "Perform_KFold_CV", "Perform_Mean_KFold_CV", "Evaluate_Models"]
+
+from .AdaBoost import (AdaBoost)
+from .ModifiedAdaBoost import (AdaBoostTunedDT, AdaBoostPerceptron)
+from .DataPreprocessing import (Fetch_Dataset, Fetch_X_y)
+from .DataVisualization import (Plot_Scatterplots, Display_Confusion_Matrix, Plot_ROC_Curve, Plot_Model_Stats, Model_Accuracies_Per_Dataset, Plot_Critial_Difference_Diagram)
+from .ModelEvaluation import (Save_json_file, Load_json_file, Manage_Results, Perform_KFold_CV, Perform_Mean_KFold_CV, Evaluate_Models)
